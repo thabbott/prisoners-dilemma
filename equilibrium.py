083d646291e1674afa9822074ff39376fa4b56b3
Tristan Abbott <tabbott0312@gmail.com>
2019-09-29 19:48:16 -0400

    Update equilibrium.py

--- equilibrium.py
+++ equilibrium.py
@@ -6,16 +6,6 @@ from TitForTat import TitForTat
 # Create tournament
 species = [Jesus, Lucifer, TitForTat]
 tournament = Tournament(species, 200, 10)
-
-# Validate species and stop if any are invalid
-valid = True
-for sp in species:
-    success = tournament.test_species(sp)
-    if not success:
-        print("%s failed validation" % sp.__name__)
-        valid = False
-if not valid:
-    exit()
         
 # Run tournament
 print(tournament)
