387843963906f0f5ecc43b6a953ab20ebc0267ab
Tristan Abbott <tabbott0312@gmail.com>
2019-09-29 19:40:29 -0400

    Update equilibrium.py

--- equilibrium.py
+++ equilibrium.py
@@ -3,10 +3,22 @@ from Jesus import Jesus
 from Lucifer import Lucifer
 from TitForTat import TitForTat
 
+# Create tournament
 species = [Jesus, Lucifer, TitForTat]
 tournament = Tournament(species, 200, 10)
-print(tournament)
 
+# Validate species and stop if any are invalid
+valid = True
+for sp in species:
+    success = tournament.test_species(sp)
+    if not success:
+        print("%s failed validation" % sp.__name__)
+        valid = False
+if not valid:
+    exit()
+        
+# Run tournament
+print(tournament)
 while True:
     inp = input("Press Enter to start the next round")
     if inp.lower() == "done":
