9eca1c4e7c28fa218da24e8c53a1d2332575cc45
Tristan Abbott <tabbott0312@gmail.com>
2019-09-29 17:35:16 -0400

    Create play_tournament.py

---
+++ play_tournament.py
@@ -0,0 +1,8 @@
+from Tournament import Tournament
+from Jesus import Jesus
+from Lucifer import Lucifer
+from TitForTat import TitForTat
+
+species = [Jesus, Lucifer, TitForTat]
+tournament = Tournament(species, 200, 10)
+tournament
