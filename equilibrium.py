8c1adba74c436c7014ef247e53447e0606d4ef00
Tristan Abbott <tabbott0312@gmail.com>
2019-09-29 19:13:49 -0400

    Create equilibrium.py

---
+++ equilibrium.py
@@ -0,0 +1,19 @@
+from Tournament import Tournament
+from Jesus import Jesus
+from Lucifer import Lucifer
+from TitForTat import TitForTat
+
+species = [Jesus, Lucifer, TitForTat]
+tournament = Tournament(species, 200, 10)
+print(tournament)
+
+while True:
+    inp = input("Press Enter to start the next round")
+    if inp.lower() == "done":
+        break
+    print("\n")
+    tournament.round_robin()
+    tournament.evaluate_fitness()
+    tournament.repopulate()
+    print("Results:")
+    print(tournament)
