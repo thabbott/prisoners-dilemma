9efb0083611e54013ef4c6c9a2d3256d35cc2cc4
Tristan Abbott <tabbott0312@gmail.com>
2019-09-29 19:12:27 -0400

    Update and rename play_tournament.py to disaster.py

--- play_tournament.py
+++
@@ -1,8 +0,0 @@
-from Tournament import Tournament
-from Jesus import Jesus
-from Lucifer import Lucifer
-from TitForTat import TitForTat
-
-species = [Jesus, Lucifer, TitForTat]
-tournament = Tournament(species, 200, 10)
-tournament
