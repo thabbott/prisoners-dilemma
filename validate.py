8f7ce8bbf760b61efe6768945f6bcb7d7e321185
Tristan Abbott <tabbott0312@gmail.com>
2019-09-30 22:15:34 -0400

    Update validate.py

--- validate.py
+++ validate.py
@@ -2,6 +2,7 @@ from Tournament import Tournament
 from Jesus import Jesus
 from Lucifer import Lucifer
 from TitForTat import TitForTat
+import time
 
 # Create tournament
 species = [Jesus, Lucifer, TitForTat]
@@ -15,11 +16,11 @@ for sp in species:
     s2 = 0
     start = time.thread_time()
     for ii in range(1000):
-        (score1, score2) = self.play_match(sp, sp, 1000)
+        (score1, score2) = tournament.play_match(sp, sp, 1000)
         s1 += score1
         s2 += score2
     end = time.thread_time()
-    print("Validating %s" % species.__name__)
+    print("Validating %s" % sp.__name__)
     print("Scores against self: %d, %d" % (s1, s2))
     print("Time: %.2f ms" % (end - start))
     valid = valid and (end - start < 100)
